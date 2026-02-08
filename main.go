e688e3d9a1840a9e56a7061541e857223f3c51c6
Slava Mihaelov <huskyslava@gmail.com>
2026-02-08 23:19:17 +0200

    comments

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 
 func updateGSheetWithJSON(data interface{}, prefix string, gsheet *[][]interface{}) {
 	switch v := data.(type) {
+	// Handle objects, recursively
 	case map[string]interface{}:
 		for k, val := range v {
 			newKey := k
@@ -36,13 +37,16 @@ func updateGSheetWithJSON(data interface{}, prefix string, gsheet *[][]interface
 			}
 			updateGSheetWithJSON(val, newKey, gsheet)
 		}
+	// Handle arrays, recursively
 	case []interface{}:
 		for i, val := range v {
 			newKey := fmt.Sprintf("%s.%d", prefix, i)
 			updateGSheetWithJSON(val, newKey, gsheet)
 		}
 	default:
+		// Append key, value to a row
 		row := []interface{}{prefix, v}
+		// Append row to a slice of rows
 		*gsheet = append(*gsheet, row)
 	}
 }
