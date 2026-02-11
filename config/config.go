983bbd729648224d7359f93b501c73aeb181678e
Slava Mihaelov <huskyslava@gmail.com>
2026-02-11 23:09:45 +0200

    config package

---
+++ config/config.go
@@ -0,0 +1,28 @@
+package config
+
+import (
+	"fmt"
+	"gopkg.in/yaml.v3"
+	"os"
+	"time"
+)
+
+type Config struct {
+	Timeout time.Duration `yaml:"timeout"`
+}
+
+func Load(path string) (*Config, error) {
+
+	cfgFile, err := os.ReadFile(path)
+	if err != nil {
+		return nil, fmt.Errorf("unable to load config: %w", err)
+	}
+
+	var cfg Config
+	err = yaml.Unmarshal(cfgFile, &cfg)
+	if err != nil {
+		return nil, fmt.Errorf("unable to parse config: %w", err)
+	}
+
+	return &cfg, nil
+}
