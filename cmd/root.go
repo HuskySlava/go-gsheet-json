d98382f816085b27019376c1405ff0d59aeb67ce
Slava Mihaelov <huskyslava@gmail.com>
2026-02-15 23:40:24 +0200

    Load config in cmd root prerun so it can be used in any sub command

--- cmd/root.go
+++ cmd/root.go
@@ -2,12 +2,24 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"go-sheet-json/config"
+	"log"
 	"os"
 )
+
+var cfg *config.Config
 
 var rootCmd = &cobra.Command{
 	Use:   "go-sheet-json",
 	Short: "My app stuff",
+	PersistentPreRunE: func(cmd *cobra.Command, args []string) error {
+		var err error
+		cfg, err = config.Load("config.yaml")
+		if err != nil {
+			log.Fatal(err)
+		}
+		return nil
+	},
 }
 
 func Execute() {
